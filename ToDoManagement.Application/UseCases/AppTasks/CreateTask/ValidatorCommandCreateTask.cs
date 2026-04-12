45d4e718e908618d48d6851401abf58f97646c00
Edwin Marshall <edwin.lozanoe@outlook.com>
2026-04-12 15:05:41 -0600

    Adds fluentvalidation package to validate CommandCreateCategory

---
+++ ToDoManagement.Application/UseCases/AppTasks/CreateTask/ValidatorCommandCreateTask.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace ToDoManagement.Application.UseCases.AppTasks.CreateTask;
+
+public class ValidatorCommandCreateTask: AbstractValidator<CommandCreateTask>
+{
+    public ValidatorCommandCreateTask()
+    {
+        RuleFor(prop => prop.Name)
+            .NotEmpty().WithMessage("El campo {PropertyName} es requerido");
+    }
+}
---
+++ ToDoManagement.Application/UseCases/Categories/CreateCategory/ValidatorCommandCreateCategory.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace ToDoManagement.Application.UseCases.Categories.CreateCategory;
+
+public class ValidatorCommandCreateCategory: AbstractValidator<CommandCreateCategory>
+{
+    public ValidatorCommandCreateCategory()
+    {
+        RuleFor(prop => prop.Name)
+            .NotEmpty().WithMessage("El campo {PropertyName} es querido");
+    }
+}
