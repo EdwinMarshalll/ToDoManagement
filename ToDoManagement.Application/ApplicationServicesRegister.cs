d6d82ed3f8a9b2cef05ea51444ed2d126f3c8c46
Edwin Marshall <edwin.lozanoe@outlook.com>
2026-04-18 14:42:27 -0600

    Add class ApplicationServicesRegister for register application services in Web Api Layer

---
+++ ToDoManagement.Application/ApplicationServicesRegister.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ToDoManagement.Application.UseCases.Categories.CreateCategory;
+using ToDoManagement.Application.UseCases.Categories.Queries.GetDetailCategory;
+using ToDoManagement.Application.Utilities.Mediator;
+
+namespace ToDoManagement.Application;
+
+public static class ApplicationServicesRegister
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddScoped<IMediator, SimpleMediator>();
+
+        services.AddScoped<IRequestHandler<CreateCategoryCommand, Guid>, 
+            UseCaseCreateCategory>();
+
+        services.AddScoped<IRequestHandler<GetDetailCategoryQuery, DetailCategoryDTO>,
+            UseCaseGetDetailCategory>();
+
+        return services;
+    }
+}
