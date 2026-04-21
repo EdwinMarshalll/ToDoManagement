fb3e59c94169f80591b08413c596cb5d3558691a
Edwin Marshall <edwin.lozanoe@outlook.com>
2026-04-20 21:23:26 -0600

    Adds EntityFrameworkCore SQL and add a Category Entity Configuration

---
+++ ToDoManagement.Persistence/Configurations/CategoryConfig.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ToDoManagement.Domain.Entities;
+
+namespace ToDoManagement.Persistence.Configurations;
+
+public class CategoryConfig : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.Property(prop => prop.Name)
+            .HasMaxLength(150)
+            .IsRequired();
+    }
+}
---
+++ ToDoManagement.Persistence/PersistenceServicesRegister.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ToDoManagement.Persistence;
+
+public static class PersistenceServicesRegister
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services)
+    {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer("name=ToDoManagementConnectionString");
+        });
+
+        return services;
+    }
+}
