e60e5f188b2b154f3952c01fb6a5aa008f0625b8
Edwin Marshall <edwin.lozanoe@outlook.com>
2026-04-22 15:33:11 -0600

    Adds CategoriesControllers and the create action. Add CreateCategoryDto class

---
+++ ToDoManagement.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoManagement.Api.DTOs.Categories;
+using ToDoManagement.Application.UseCases.Categories.CreateCategory;
+using ToDoManagement.Application.Utilities.Mediator;
+
+namespace ToDoManagement.Api.Controllers;
+
+[ApiController]
+[Route("api/categories")]
+public class CategoriesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CategoriesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post(CreateCategoryDto createCategoryDto)
+    {
+        var command = new CreateCategoryCommand() { Name = createCategoryDto.Name };
+        await _mediator.Send(command);
+        return Ok();
+    }
+}
---
+++ ToDoManagement.Api/DTOs/Categories/CreateCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoManagement.Api.DTOs.Categories;
+
+public class CreateCategoryDto
+{
+    [Required]
+    [StringLength(150)]
+    public required string Name { get; set; }
+}
