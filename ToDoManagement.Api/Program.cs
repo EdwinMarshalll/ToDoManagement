7270f86fd54be69aaa1dcda552ad1256ea3edf43
Edwin Marshall <edwin.lozanoe@outlook.com>
2026-04-22 15:17:47 -0600

    Add project dependencies

--- ToDoManagement.Api/Program.cs
+++ ToDoManagement.Api/Program.cs
@@ -1,3 +1,6 @@
+using ToDoManagement.Application;
+using ToDoManagement.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddApplicationServices();
+builder.Services.AddPersistenceServices();
 
 var app = builder.Build();
 
